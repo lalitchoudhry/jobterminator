2aff439ac635725304176b9b33d1081f64e6144c
Lalit <choudharylalit182@gmail.com>
2023-04-14 15:45:27 +0530

    added database config and initialzation

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const port = process.env.PORT || 80;
 
 const app = express()
+app.use(express.json())
 
+// connect to database
+const url = process.env.DATABASE_URL
+mongoose.connect(url)
+.then(()=>console.log('Database connected to server'))
+.catch((err)=>console.log(err))
+
+// routes
 app.get('/', (req, res)=>{
     res.send('hello');
 })
